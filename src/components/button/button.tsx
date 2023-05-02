bcc11b6ad33c897c265cb224a5935555cb5a982e
williamchun1999 <williamchun1999@gmail.com>
2023-05-01 18:25:59 -0700

    added tailwindcss and DaisyUI libraries for our styling

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import './button.module.css'
 export const Button = () => {
   const [count, setCount] = useState(0);
+
   return (
-    <button style={{color: 'blue'}} onClick={() => setCount((count) => count + 1)}>
-    count is {count}
-  </button>
+    <button onClick={() => setCount(count + 1)} className="btn">count is {count}</button>
+
   )
 }
 
