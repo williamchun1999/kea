125e7002a028686d1a8b38c8847a211d00f4be12
williamchun1999 <williamchun1999@gmail.com>
2023-05-09 16:58:20 -0700

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <MainPanel />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+        children: [
+          {
+            index: true,
+            element: <ProfileCard />,
+          },
+          {
+            path: "edit",
+            element: <EditProfile />,
+          },
+          {
+            path: "settings",
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
