93233fee833ca7355550d24299fd7cc4beb1bf24
williamchun1999 <williamchun1999@gmail.com>
2023-05-10 00:02:10 -0700

    unsaved changes of route

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-<<<<<<< HEAD
 import Home from './views/home.tsx'
-=======
-import App from './App.tsx'
->>>>>>> 125e7002a028686d1a8b38c8847a211d00f4be12
 import './index.css'
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-<<<<<<< HEAD
     element: <Home />,
     // errorElement: <ErrorPage />,
     children: [
@@ -21,32 +16,6 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     
-=======
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <MainPanel />,
-      },
-      {
-        path: "profile",
-        element: <Profile />,
-        children: [
-          {
-            index: true,
-            element: <ProfileCard />,
-          },
-          {
-            path: "edit",
-            element: <EditProfile />,
-          },
-          {
-            path: "settings",
-          },
-        ],
-      },
->>>>>>> 125e7002a028686d1a8b38c8847a211d00f4be12
     ],
   },
 ]);
