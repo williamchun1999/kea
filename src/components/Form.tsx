318e0a93952c449b9642117349c8ffcafcffd8ee
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-05-15 11:45:07 -0300

    modify the signup button for alignment

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,7 +12,7 @@ type formType= {
 
 export const TextBox = (form : formType) => {
   return (
-    <div className="m-auto w-8/12">
+    <div className="w-8/12">
         <p className="pb-2">{form.title}</p>
         <input type="text" placeholder={form.title} name = {form.name} className="input input-bordered input-primary w-full max-w-xs" onChange= {form.controlInput} value={form.value} />
     </div>
--- src/views/signUp.tsx
+++ src/views/signUp.tsx
@@ -35,13 +35,15 @@ function signUp() {
         <div className="div bg-base-200 h-screen">
             <div className="flex flex-col gap-5">
             <h1 className="text-5xl mt-20 ml-10 tracking-wider">Sign Up</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
                 <TextBox title="First Name" name="firstName" value={formData.firstName}  controlInput = {handleChange}/> 
                 <TextBox title="Last Name" name="lastName" value={formData.lastName} controlInput = {handleChange}/> 
                 <TextBox title="Email" name="email" value={formData.email}  controlInput = {handleChange}/> 
                 <TextBox title="Password" name="password" value={formData.password}  controlInput = {handleChange}/> 
                 <TextBox title="Password Confirmation" name="passwordC" value={formData.passwordC} controlInput = {handleChange} /> 
-                <Button name="Sign Up" color="bg-primary" />
+                <div className="mt-10">
+                    <Button name="Sign Up" color="bg-primary" />
+                </div>
             </form>
             </div>
         </div>
