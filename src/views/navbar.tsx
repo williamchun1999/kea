7db4404f0ff6c889c77208e935c3600f4b9d484e
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-05-18 16:49:48 -0300

    comments

--- src/views/navbar.tsx
+++ src/views/navbar.tsx
@@ -4,6 +4,7 @@ import { NavLink, Outlet } from "react-router-dom";
 import { NavButton } from "../components/button/button";
 
 export const Navbar = () => {
+  //data for the buttons 
   const buttons=[
     {
       d: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6",
