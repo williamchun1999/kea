82ca58f02d529f46e52f24106c6defe990943841
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-05-19 11:41:04 -0300

    add username portion under login

--- src/views/signUp.tsx
+++ src/views/signUp.tsx
@@ -10,6 +10,7 @@ export const SignUp=()=> {
     const [formData, setFormData] = useState({
         firstName:"",
         lastName:"",
+        userName:"",
         email:"",
         password:"",
         passwordC:""
@@ -33,11 +34,12 @@ export const SignUp=()=> {
     }
     return (
         <div className="div bg-base-200 h-screen">
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-5 bg-base-200">
             <h1 className="text-5xl mt-10 ml-10 tracking-wider">Sign Up</h1>
             <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
                 <TextBox title="First Name" name="firstName" value={formData.firstName}  controlInput = {handleChange}/> 
                 <TextBox title="Last Name" name="lastName" value={formData.lastName} controlInput = {handleChange}/> 
+                <TextBox title=" username" name="userName" value={formData.userName} controlInput = {handleChange}/> 
                 <TextBox title="Email" name="email" value={formData.email}  controlInput = {handleChange}/> 
                 <TextBox title="Password" name="password" value={formData.password}  controlInput = {handleChange}/> 
                 <TextBox title="Password Confirmation" name="passwordC" value={formData.passwordC} controlInput = {handleChange} /> 
