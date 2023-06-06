b3c397fed833d908465ac4a62cb03ea67639f028
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-06-06 16:15:24 +0900

    added breakpoints for signup page

--- client/src/views/signUp.tsx
+++ client/src/views/signUp.tsx
@@ -1,61 +1,116 @@
-import { ChangeEventHandler, FormEventHandler , useState  } from 'react';
-import {Link} from "react-router-dom"
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { TextBox } from "../components/Form";
-import { Button } from "../components/button/button";
+import { TextBox } from '../components/Form';
+import { Button } from '../components/button/button';
+
+export const SignUp = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    password: '',
+    passwordC: '',
+  });
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="div bg-base-200 h-screen flex justify-center items-center">
+      <div className="flex flex-col bg-base-200 ">
+        <h1 className="text-5xl mb-10 tracking-wider text-center">Sign Up</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-screen flex-wrap justify-center md:justify-start md:gap-2"
+        >
+
+        <div className="top md:flex md:flex-col md:items-center md:w-screen md:justify-center md:gap-4">
+            <div className="firstrow md:flex">
+            <div className="flex flex-col gap-4">
+            <TextBox
+                title="First Name"
+                name="firstName"
+                value={formData.firstName}
+                controlInput={handleChange}
+                type="text"
+            />
+
+            
+            </div>
+            <div className="flex flex-col gap-4">
+            <TextBox
+                title="Last Name"
+                name="lastName"
+                value={formData.lastName}
+                controlInput={handleChange}
+                type="text"
+            />
+            
+            </div>
 
 
-export const SignUp=()=> {
+            </div>
+            <div className="secondrow md:flex">
+            <TextBox
+                title="Email"
+                name="email"
+                value={formData.email}
+                controlInput={handleChange}
+                type="text"
+            />
+            <TextBox
+                title="Username"
+                name="userName"
+                value={formData.userName}
+                controlInput={handleChange}
+                type="text"
+            />
+            </div>
+            <div className="thirdrow md:flex">
+            <TextBox
+                title="Password"
+                name="password"
+                value={formData.password}
+                controlInput={handleChange}
+                type="password"
+            />
+            <TextBox
+                title="Password Confirmation"
+                name="passwordC"
+                value={formData.passwordC}
+                controlInput={handleChange}
+                type="password"
+            />
+            </div>
+            
+        </div>
 
-    const [formData, setFormData] = useState({
-        firstName:"",
-        lastName:"",
-        userName:"",
-        email:"",
-        password:"",
-        passwordC:""
-    })
-  
-    //make controlled inputs from React's end
-    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-        setFormData(prev => {
-            return{
-                ...prev,
-                [event.target.name]: event.target.value
-            }
-        })
-    }
-
-    //on pressing the signup button, the info is gathered and sent to db 
-     const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-        event.preventDefault()
-        //write function to send to the db , for now just console log's the data that was inputted 
-        console.log(formData)
-    }
-    return (
-        <div className="div bg-base-200 h-screen">
-            <div className="flex flex-col gap-5 bg-base-200">
-            <h1 className="text-5xl mt-10 ml-10 tracking-wider">Sign Up</h1>
-            <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
-                <TextBox title="First Name" name="firstName" value={formData.firstName}  controlInput = {handleChange}/> 
-                <TextBox title="Last Name" name="lastName" value={formData.lastName} controlInput = {handleChange}/> 
-                <TextBox title=" username" name="userName" value={formData.userName} controlInput = {handleChange}/> 
-                <TextBox title="Email" name="email" value={formData.email}  controlInput = {handleChange}/> 
-                <TextBox title="Password" name="password" value={formData.password}  controlInput = {handleChange}/> 
-                <TextBox title="Password Confirmation" name="passwordC" value={formData.passwordC} controlInput = {handleChange} /> 
-                <div className="mt-10 flex flex-col">
+        <div className="bottom mt-10 w-screen">
+            <div className="flex flex-col items-center">
                 <Link to="/home">
                     <Button name="Sign Up" color="bg-primary" />
                 </Link>
                 <Link to="/login">
                     <Button name="Login" color="bg-primary" />
                 </Link>
-                </div>
-            </form>
             </div>
         </div>
-    )
-}
-
-  
-  
+       
+        </form>
+      </div>
+    </div>
+  );
+};
