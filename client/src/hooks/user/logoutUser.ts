39f25c291207ca0901b34a3f6ab1f41c41691d76
williamchun1999 <williamchun1999@gmail.com>
2023-06-14 14:46:44 -0700

    fixed login/logout issues due to accidental link element

--- client/src/hooks/user/logoutUser.ts
+++ client/src/hooks/user/logoutUser.ts
@@ -1,28 +1,22 @@
-import { AxiosRequestConfig, isAxiosError } from "axios";
-import { axiosInstance  } from "../../axios";
-
-
+import { AxiosRequestConfig, AxiosResponse, isAxiosError } from "axios";
+import { axiosInstance } from "../../axios";
 
 export const useUserLogout = async (
   url: string,
   axiosConfigOptions?: AxiosRequestConfig
-):Promise<any> => {
-    try {
-      const response = await axiosInstance.get(url, axiosConfigOptions)
-      console.log(response.data)
-      console.log('response status is: ', response.status);
-      return response.data;
-    }
-    catch (err) {
-      if (isAxiosError(err)) {
-        console.log('error message: ', err.message);
-        return err.message;
-      }
-      else {
-        console.log('unexpected error: ', err);
-        return 'An unexpected error occured';
-      }
+): Promise<AxiosResponse | null> => {
+  try {
+    const response = await axiosInstance.get(url, axiosConfigOptions);
+    console.log(response.data);
+    console.log("response status is: ", response.status);
+    return response;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      console.log("Axios error: ", err);
+      return null;
+    } else {
+      console.log("unexpected error: ", err);
+      return null;
     }
   }
-  
-        
+};
