bc00e62eac751f58af5548ae4e7f1068e5d92039
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-07-10 19:28:40 -0300

    change the landing page

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -8,29 +8,53 @@ type Images = {
 
 export const Carousel = (imgs: Images) => {
   return (
-    <div className="w-3/4 m-auto carousel rounded-box">
+    <>
+    <div className="w-3/4 m-auto carousel rounded-box lg:hidden">
         <div className="carousel-item w-full">
           <img
             src={imgs.img1}
             className="w-full"
-            alt="Tailwind CSS Carousel component"
+            alt="homepage"
           />
         </div>
         <div className="carousel-item w-full">
           <img
             src={imgs.img3}
             className="w-full"
-            alt="Tailwind CSS Carousel component"
+            alt="profile page"
           />
         </div>
         <div className="carousel-item w-full">
           <img
             src={imgs.img2}
             className="w-full"
-            alt="Tailwind CSS Carousel component"
+            alt="friends page"
           />
         </div>
-  
       </div>
+      <div className= "hidden lg:flex items-center gap-10 justify-center">
+        <div className="w-1/5">
+        <img
+            src={imgs.img1}
+            className="w-full"
+            alt="homepage"
+          />
+        </div>
+        <div className="w-1/5">
+        <img
+            src={imgs.img3}
+            className="w-full"
+            alt="profile page"
+          />
+        </div>
+        <div className="w-1/5">
+        <img
+            src={imgs.img2}
+            className="w-full"
+            alt="friends page"
+          />
+        </div>
+      </div>
+      </>
   )
 }
--- client/src/views/landingPage.tsx
+++ client/src/views/landingPage.tsx
@@ -12,11 +12,11 @@ import profile from "../assets/profile.png";
 export const LandingPage= ()=> {
   
   return (
-    <div className="bg-base-200 flex flex-col h-screen w-screen">
+    <div className="bg-base-200 flex flex-col h-full w-screen">
       <MainTitle />
       <Carousel img1={homepage} img2={friends} img3={profile}/>
       <p className=" w-4/5 text-center m-auto text-secondary "> Keep yourself and your friends accountable through this app!</p>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center ">
         <Link to="/login">
           <Button name="Login" color="bg-primary "/>
         </Link>
